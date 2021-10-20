9d8abe629e8eae096698b0b92654aea28c86cd58
Eshan <eshanwells@gmail.com>
2021-10-20 22:30:40 +0100

    First try

--- ACCSharedMemoryRelay.py
+++ ACCSharedMemoryRelay.py
@@ -1,3 +1,37 @@
 """
 
 """
+
+from dataclasses import dataclass
+
+@dataclass
+class SPageFilePhysics:
+    packetId:           int
+    gas:                float
+    brake:              float
+    fuel:               float
+    gear:               int
+    rpm:                int
+    steerAngle:         float
+    speedKMH:           float
+    velocity:           float * 3
+    accG:               float * 3
+    wheelSlip:          float * 3
+
+@dataclass
+class SPageFileStatic:
+    smVersion:          str
+    acVersion:          str
+    numberOfSessions:   str
+    numCars:            int
+    carModel:           str
+
+@dataclass
+class Stefan:
+    name: str = "stoyfan1"
+    age: float = 69.420
+    message: str = "hi John's mum"
+
+print(Stefan.name)
+print(Stefan.age)
+print(Stefan.message)
